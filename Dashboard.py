a2a35db4e21e828b960203a9d367cfc5e8aecd49
Remi Cazelles <remi.cazelles.fr@gmail.com>
2023-06-19 11:56:08 +0200

    move dependency inside main() to avoid installing several times

--- Dashboard.py
+++ Dashboard.py
@@ -5,11 +5,7 @@ import time
 import subprocess
 
 ### install dependencies
-with st.spinner('Installing dependencies...'):
-    subprocess.run(["streamlit", "cache", "clear"])
-    time.sleep(0.05)
-    subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-st.success("Dependencies installed successfully!")
+
 
 
 ### load processing functions
@@ -24,6 +20,13 @@ def main():
     text = st.text_area("Enter your text:")
 
     if st.button("Process Text"):
+    
+        with st.spinner('Installing dependencies...'):
+            subprocess.run(["streamlit", "cache", "clear"])
+            time.sleep(0.05)
+            subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+            st.success("Dependencies installed successfully!")
+            
         text_ready = clean_text(text)
         tokens_ready = make_tokens(text_ready)
 
